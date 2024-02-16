70524f6482449874913b608d4c460a3c16c711f9
Vibinraj7 <vibinrajkk@gmail.com>
2024-02-16 10:56:58 +0530

    firstcomit

--- dbConnect.js
+++ dbConnect.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
+
+
 const mongoose = require("mongoose");
 const dbConnect = ()=>{
     try {
-        const connect = mongoose.connect("mongodb://127.0.0.1:27017/REPSUP_LOGIN");
+        const connect = mongoose.connect(process.env.DB_URI);
         console.log("database connected");
     } catch (error) {
         console.log("database error")
